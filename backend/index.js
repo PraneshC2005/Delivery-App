04751f1c8ece6e72fafc836d786a15fb8f58bc8c
Pranesh Chandrasekaran <125540554+PraneshC2005@users.noreply.github.com>
2025-09-05 12:05:26 +0530

    bug

--- backend/index.js
+++ backend/index.js
@@ -18,14 +18,14 @@ const app=express()
 const server=http.createServer(app)
 const io=new Server(server,{
      cors: {
-    origin: "http://localhost:5173", // production में specific domain डालना
+    origin: "https://delivery-app-caf9.vercel.app", // production में specific domain डालना
     methods: ["GET", "POST"],
     credentials: true  
   }
 })
 app.set("io", io);
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://delivery-app-caf9.vercel.app",
     credentials:true
 }))
 app.use(express.json())
